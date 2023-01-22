0fd2892ebf8908c95218191b54482106a6edc502
takaya suzuki <suzuki8522@yahoo.co.jp>
2023-01-22 17:07:50 +0900

    🔧useEffect使用

--- src/010_useEffect_init/start/Example.js
+++ src/010_useEffect_init/start/Example.js
@@ -1,5 +1,20 @@
+import { useEffect, useState } from "react";
+
 const Example = () => {
-  return <></>;
+  const [time, setTime] = useState(0);
+
+  useEffect(() => {
+    window.setInterval(() => {
+      setTime((prev) => prev + 1);
+    }, 1000);
+  }, [])
+
+  return (
+    <h3>
+      <time>{time}</time>
+      <span>秒経過</span>
+    </h3>
+  );
 };
 
 export default Example;
