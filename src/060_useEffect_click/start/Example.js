5fec02bb1b9c417a6d0a3c904cc54f41865cd4e4
takaya suzuki <suzuki8522@yahoo.co.jp>
2023-01-23 17:34:32 +0900

    🔧useEffect Click追加

--- src/060_useEffect_click/start/Example.js
+++ src/060_useEffect_click/start/Example.js
@@ -5,53 +5,72 @@ const Example = () => {
 
   return (
     <>
-      {isDisp && <Timer/>}
-      <button onClick={() => setIsDisp(prev => !prev)}>トグル</button>
+      {isDisp && <Timer />}
+      <button onClick={() => setIsDisp((prev) => !prev)}>
+        {isDisp ? "非表示" : "表示"}
+      </button>
     </>
-  )
-}
+  );
+};
 
 const Timer = () => {
   const [time, setTime] = useState(0);
+  const [isRunning, setIsRunning] = useState(false);
 
   useEffect(() => {
     // console.log('init');
     let intervalId = null;
-    intervalId = window.setInterval(() => {
-      // console.log('interval running');
-      setTime(prev => prev + 1);
-    }, 1000);
-    return () => {
-      window.clearInterval(intervalId)
-      // console.log('end');
+    if (isRunning) {
+      intervalId = window.setInterval(() => {
+        // console.log('interval running');
+        setTime((prev) => prev + 1);
+      }, 1000);
     }
-  }, [])
-  
+    return () => {
+      window.clearInterval(intervalId);
+      // console.log('end');
+    };
+  }, [isRunning]);
+
   useEffect(() => {
     // console.log('updated');
-    
-    document.title = 'counter:' + time;
-    window.localStorage.setItem('time-key', time);
+
+    document.title = "counter:" + time;
+    window.localStorage.setItem("time-key", time);
 
     return () => {
       // debugger
       // console.log('updated end');
-    }
+    };
   }, [time]);
 
   useLayoutEffect(() => {
-    const _time = parseInt(window.localStorage.getItem('time-key'));
-    if(!isNaN(_time)) {
+    const _time = parseInt(window.localStorage.getItem("time-key"));
+    if (!isNaN(_time)) {
       setTime(_time);
     }
-  }, [])
+  }, []);
+
+  const toggle = () => {
+    setIsRunning((prev) => !prev);
+  };
+  const reset = () => {
+    setTime(0);
+    setIsRunning(false);
+  };
 
   return (
-    <h3>
-      <time>{time}</time>
-      <span>秒経過</span>
-    </h3>
-    );
+    <>
+      <h3>
+        <time>{time}</time>
+        <span>秒経過</span>
+      </h3>
+      <div>
+        <button onClick={toggle}>{isRunning ? "一時停止" : "スタート"}</button>
+        <button onClick={reset}>リセット</button>
+      </div>
+    </>
+  );
 };
 
 export default Example;
