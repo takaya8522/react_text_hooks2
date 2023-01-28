224a7b7a40ca11f0cacc68b54f064c022d0e2122
takaya suzuki <suzuki8522@yahoo.co.jp>
2023-01-28 11:59:38 +0900

    🔧不要コード削除

--- src/075_practice_customHook/start/Example.js
+++ src/075_practice_customHook/start/Example.js
@@ -1,11 +1,6 @@
 const Example = () => {
   return (
     <>
-      <h3>練習問題</h3>
-      <p>
-        記述を変更し、完成コードと同じ状態になるようにしてください。
-        startフォルダの中にhooks.jsというファイルを作成しその中でuseCountというカスタムフックを作成してください。
-      </p>
       <div>Counts: {}</div>
       <button onClick={() => {}}>Count Up!</button>
     </>
